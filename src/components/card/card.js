9f3e33d0674d80446d4fa3be0f8407ef2c44fc54
mariana549 <marievaristo889@gmail.com>
2024-03-08 10:33:12 -0300

    iniciando projeto

---
+++ src/components/card/card.js
@@ -0,0 +1,10 @@
+
+const card = () => {
+   return (
+      <>
+      <p className="paragrafro">
+         lorem ipsum dolor sit amet, consectetur adip euismod tempor incididunt ut labore et dolore magna aliqu fugiat nulla pariatur et accus. 
+      </p>
+      </>
+   )
+}
