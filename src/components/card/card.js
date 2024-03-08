c122676b528216b4a1d0c032386be4b66b367c4f
mariana549 <marievaristo889@gmail.com>
2024-03-08 10:52:17 -0300

    adicionando estilo ao paragrafro

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,10 +1,18 @@
+import "../card/card.css"
 
-const card = () => {
+const Card = ({color, styleText}) => {
    return (
       <>
-      <p className="paragrafro">
+      <p className="paragrafro" style={{backgroundColor: color, textTransform: styleText}}>
          lorem ipsum dolor sit amet, consectetur adip euismod tempor incididunt ut labore et dolore magna aliqu fugiat nulla pariatur et accus. 
       </p>
       </>
    )
 }
+
+Card.defaultProps = {
+   color: "crimson",
+   styleText: "uppercase"
+}
+
+export default Card
